f8863ff4f44c7734d1cc5580466ebdc49541044d
chen <1728533217en@gmail.com>
2018-06-20 15:22:23 +0800

    poor solution cause of overflow

--- Smallest_Good_Base.cpp
+++ Smallest_Good_Base.cpp
@@ -4,45 +4,116 @@
 #include<algorithm>
 #include<vector>
 using namespace std;
+unsigned long long int mypow(unsigned long long int base, size_t pow)
+{
+	unsigned long long int temp = 1;
+	for (int i = 0; i < pow; ++i)
+	{
+		temp *= base;
+	}
+	return temp;
+}
+
+unsigned long long int get_n_at_base_with_l(unsigned long long int base, size_t len)
+{
+	unsigned long long int sum = 1;
+	for (size_t i = 1; i < len; ++i)
+	{
+		unsigned long long int temp = mypow(base, i);
+		if (std::numeric_limits< unsigned long long >::max() - temp < sum)
+		{
+			break;
+		}
+		else
+			sum += temp;
+	}
+	return sum;
+}
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+    	unsigned long long int x = std::stoll(n);
+    	unsigned long long int y=2;
+    	unsigned long long int value=0;
+
+    	size_t max_pow=std::ceil(std::log(x)/std::log(2));
+    	size_t i = max_pow;
+    	for (; i > 0; --i)
+    	{
+    		unsigned long long int begin = y;
+    		unsigned long long int end = std::numeric_limits< unsigned long long >::max() - y;	
+    		
+    		while(begin <= end)
+    		{
+
+    			y = (begin+end)/2;
+    			value = get_n_at_base_with_l(y, i);
+    			if (value > x)
+    			{
+    				end = y-1;
+    			}
+    			else if ( value == x)
+    			{
+    				break;
+    			}
+    			else if ( value < x)
+    			{
+    				begin = y+1;
+    			}
+    		}
+    		if (value == x)
+    			break;
+    	}    	
+        return std::to_string(y);
+    }
+};
+
+
 int main(int argc, char const *argv[])
 {
 	//std::vector<int> nums={1,3,6,2,5,8,7,11};
-	std::string s="4681";
-	unsigned long long int x = std::stoll(s);
+	std::string n="2251799813685247";
+	unsigned long long int x = std::stoll(n);
 	unsigned long long int y=2;
 	unsigned long long int value=0;
 
-	while(true)
-	{	
-		value = 0;
-		if ((x-1)%y != 0)
+	size_t max_pow=std::ceil(std::log(x)/std::log(2));
+	size_t i = max_pow;
+	for (; i > 0; --i)
+	{
+
+		std::cout<<i<<"------!\n";
+		std::cout<<std::pow(x, 1.0/i)<<"\t"<<std::pow(x,1.0/(i-1))<<std::endl;
+		unsigned long long int begin = y;
+		unsigned long long int end = std::numeric_limits< unsigned long long >::max() - y;	
+		
+		while(begin <= end)
 		{
-			// std::cout<<"hit once"<<std::endl;
-			++y;
-			continue;
-		}
-		size_t max_pow=std::ceil(std::log(x)/std::log(y));
-		if (pow(y,max_pow) < x)
-		{
-			++y;
-			continue;
-		}
-		std::cout<<y<<std::endl;		
-		for (int i = 0; i <= max_pow ; ++i)
-		{
-			value += pow(y,i);
-			if (value >= x)
+
+			y = (begin+end)/2;
+			if (y == 1125899906842624 and i == 52)
+			{
+				std::cout<<" break at here\n";
+			}
+			std::cout<<begin<<"\t"<<y<<"\t"<<end<<std::endl;
+			value = get_n_at_base_with_l(y, i);
+			std::cout<<y<<"\t"<<value<<std::endl<<std::endl;
+			if (value > x)
+			{
+				end = y-1;
+			}
+			else if ( value == x)
 			{
 				break;
 			}
+			else if ( value < x)
+			{
+				begin = y+1;
+			}
 		}
-		if (value == x)		
+		if (value == x)
 			break;
-		else
-			++y;
-
-		
 	}
-	std::cout<<y<<"\t"<<value;
+	std::cout<<i<<"\t"<<y<<"\t"<<value;
 	return 0;
 }
