50ab9b91baf2d57ef28006933ff4d615409cbbd9
chen <1728533217en@gmail.com>
2018-06-30 19:31:24 +0800

    update

--- 125_Valid_Palindrome.cpp
+++ 125_Valid_Palindrome.cpp
@@ -1,21 +1,38 @@
 #include<iostream>
 #include<string>
+#include<vector>
 #include<cctype>
 using namespace std;
 class Solution {
 public:
     bool isPalindrome(string s) {
+        vector<int> lr,rl;
         for(auto e : s)
         {
-            cout<<isalpha(e)<<"#"<<e<<endl;
+            if(isalnum(e))
+            {
+                lr.push_back(toupper(e));
+            }
         }
-            
-        return 1;       
+        for(int j = s.size()-1; j >= 0;j--)
+        {
+            if(isalnum(s[j]))
+            {
+                rl.push_back(toupper(s[j]));
+            }
+        }
+        for(size_t i = 0; i < lr.size();i++)
+        {
+            //cout<<lr[i]<<"\t"<<rl[i]<<endl;
+            if(lr[i] != rl[i])
+                return false;
+        }
+        return true;       
     }
 };
 
 int main(int argc, const char* argv[])
 {
     Solution s;
-    cout<<s.isPalindrome("A man, a plan, a canal: Panama");
+    cout<<s.isPalindrome("0P");
 }
