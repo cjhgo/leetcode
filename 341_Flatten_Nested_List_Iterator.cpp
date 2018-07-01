96690bf436c889181cd4d4db4a28a32a8126ec01
chen <1728533217en@gmail.com>
2018-07-01 23:30:01 +0800

    update

---
+++ 341_Flatten_Nested_List_Iterator.cpp
@@ -0,0 +1,18 @@
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        
+    }
+
+    int next() {
+        
+    }
+
+    bool hasNext() {
+        
+    }
+};
+int main(int argc, char* argv[])
+{
+    
+}
