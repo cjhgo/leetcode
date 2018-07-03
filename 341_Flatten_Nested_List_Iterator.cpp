f70ca2e7cb98e4df59a32cdbde98caa08e503c66
chen <1728533217en@gmail.com>
2018-07-03 22:35:40 +0800

    update

--- 341_Flatten_Nested_List_Iterator.cpp
+++ 341_Flatten_Nested_List_Iterator.cpp
@@ -1,3 +1,21 @@
+
+class NestedInteger {
+    int e_int;
+    vector<int> e_vec;    
+public:
+
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+
 class NestedIterator {
 public:
     NestedIterator(vector<NestedInteger> &nestedList) {
@@ -14,5 +32,8 @@ public:
 };
 int main(int argc, char* argv[])
 {
-    
+
+    NestedIterator i(nestedList);
+    while (i.hasNext()) 
+        cout << i.next();
 }
