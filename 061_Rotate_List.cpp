197d7d22cdd64dcd8b6a5026aaff545f07afbf9e
chen <1728533217en@gmail.com>
2018-07-22 22:05:22 +0800

    not finish 061

---
+++ 061_Rotate_List.cpp
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ **/
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        
+    }
+};
