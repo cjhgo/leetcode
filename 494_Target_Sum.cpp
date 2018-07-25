cda0e70b6819e68f25e69de80fa905d4161be598
chen <1728533217en@gmail.com>
2018-07-25 21:53:08 +0800

    not finish 494

---
+++ 494_Target_Sum.cpp
@@ -0,0 +1,14 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        return 0;    
+    }
+};
+
+main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
