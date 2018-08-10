83619610cd80bdf922a4968739c4b85bef0b3358
chen <1728533217en@gmail.com>
2018-08-10 23:53:42 +0800

    287 ac

--- 287_Find_the_Duplicate_Number.cpp
+++ 287_Find_the_Duplicate_Number.cpp
@@ -1,15 +1,31 @@
 #include<vector>
 #include<iostream>
+#include<algorithm>
 using namespace std;
-class Solution {
+class Solution 
+{
 public:
-    int findDuplicate(vector<int>& nums) {
-        
+    int findDuplicate(vector<int>& nums) 
+    {
+        int res=-1;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size()-1; i++)
+        {
+            int j = i+1;
+            if(nums[j] == nums[i])
+            {
+                res = nums[i];
+                break;
+            }
+        }    
+        return res;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-    
+    vector<int> nums={3,1,3,4,2};
+    Solution s;
+    cout<<s.findDuplicate(nums);
     return 0;
 }
