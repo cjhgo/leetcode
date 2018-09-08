82ef8f7838a412722c9b166ed29577d883707909
chen <1728533217en@gmail.com>
2018-09-08 23:54:28 +0800

    001 ac

---
+++ 001_Two_Sum.cpp
@@ -0,0 +1,40 @@
+#include<vector>
+#include<iostream>
+#include<map>
+#include<algorithm>
+#include<iterator>
+using namespace std;
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        size_t n = nums.size();
+        vector<int> res(2,0);
+        int i,j;
+        for(i = 0;i < n; i++)
+        {
+            for( j = 0;j < n; j++)
+            {
+                if(j!=i and nums[i]+nums[j] == target)
+                {
+                    res[0]=i;
+                    res[1]=j;
+                    break;
+                }
+            }
+        }        
+        return res;
+    }
+
+};
+int main(int argc, char const *argv[])
+{
+    vector<int> nums={3,2,4};//{2,7,11,15};
+    Solution s;
+    for(auto e : s.twoSum(nums, 6))
+    {
+        cout<<e<<"\t";
+    }
+    return 0;
+}
