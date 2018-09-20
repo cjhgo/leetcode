463da39199fbcf356a19160bf09a97c39967b046
chen <1728533217en@gmail.com>
2018-09-21 00:19:53 +0800

    007 ac

--- 007_Reverse_Integer.cpp
+++ 007_Reverse_Integer.cpp
@@ -1,4 +1,6 @@
 #include<string>
+#include<cstdlib>
+#include<climits>
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -9,23 +11,26 @@ public:
     int reverse(int x) 
     {
         int  negative = 1;
-        if( x < 0)
+        int64_t xx = x;
+        if( xx < 0)
         {
-            negative = -1;
-            x = -x;
+            negative = -1;            
+            xx = -1*xx;            
         }
-        string x_s = to_string(x);
-        cout<<x_s<<endl;
-        std::reverse(x_s.begin(), x_s.end());
-        cout<<x_s<<endl;
-        int res = stoll(x_s);
-        return negative * res;
+        string x_s = to_string(xx);        
+        std::reverse(x_s.begin(), x_s.end());    
+        string int_max = to_string(INT32_MAX);
+        if(x_s.length() == int_max.length() and x_s > int_max)
+        return 0;        
+        return negative * stoi(x_s);;
     }
 };
 int main(int argc, char const *argv[])
-{
-    int x = 1534236469;    
+{    
     Solution s;
-    cout<<s.reverse(x);
+    cout<<"range " << INT32_MIN << "\t"<< INT32_MAX<<endl;
+    cout<<s.reverse(-2147483648)<<endl;
+    cout<<s.reverse(123)<<endl;
+    cout<<s.reverse(1534236469)<<endl;
     return 0;
 }
