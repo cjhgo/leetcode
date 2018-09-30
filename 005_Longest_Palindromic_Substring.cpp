4c1d7762ae1ac15ff0777d733c84480f4e4f01ca
chen <1728533217en@gmail.com>
2018-10-01 01:54:05 +0800

    not finish 005 using dp

--- 005_Longest_Palindromic_Substring.cpp
+++ 005_Longest_Palindromic_Substring.cpp
@@ -1,4 +1,6 @@
 #include<string>
+#include<vector>
+#include<iterator>
 #include<algorithm>
 #include<iostream>
 using namespace std;
@@ -7,14 +9,30 @@ class Solution
 public:
     bool is_Palindrome(string s)
     {
-        string copy(s);
-        reverse(copy.begin(), copy.end());
+        string copy;
+        reverse_copy(s.begin(),s.end(), inserter(copy,copy.begin()));
+        cout<<"\n...\n"<<s<<"...\n"<<copy<<endl;
         if( s == copy)
             return true;
         else
             return false;
     }
     string longestPalindrome(string s) 
+    {
+        size_t s_len = s.length();
+        vector<int> temp(s_len, 0);
+        size_t max_len = 0;
+        vector< vector<int> > flag(s_len, vector<int>(s_len, 0));
+        for(size_t i = 0; i < s_len;i++)
+        {
+            flag[i][i]=1;
+            if( i+1 < s_len)
+                flag[i][i+1]= (s[i] == s[i+1]);
+        }        
+        return "";
+
+    }
+    string longestPalindrome_brute_force(string s) 
     {
         size_t max_len = 0, begin = 0, end = 0;
         string res;
@@ -37,8 +55,9 @@ public:
 };
 int main(int argc, char const *argv[])
 {
-    string s("babad");    
+    string s("babab");    
     Solution sol;
     cout<<sol.longestPalindrome(s);
+    cout<<sol.is_Palindrome(s);
     return 0;
 }
