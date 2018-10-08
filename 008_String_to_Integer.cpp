510ef2533410531763d1c4daa72cc2b1f00b4fc5
chen <1728533217en@gmail.com>
2018-10-08 11:07:02 +0800

    improve 008

--- 008_String_to_Integer.cpp
+++ 008_String_to_Integer.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
-#include<cmath>
+#include<climits>
 #include<string>
-#include<sstream>
 #include<cctype>
 using namespace std;
 class Solution 
@@ -9,61 +8,29 @@ class Solution
 public:
     int myAtoi(string str) 
     {
-        size_t cur=0;
-        size_t next=cur+1;
-        char flag='+';
-        ostringstream out;
+        int plus = 1;
         size_t slen = str.length();
-        while(cur < slen and str[cur] == ' ')
-            cur++;
-        if( cur == slen ) return 0;
-        if(str[cur] == '+' || str[cur] == '-')
-        {
-            flag = str[cur];
-            ++cur;
-            while(str[cur] == '0' and cur < slen)
-            ++cur;
-            if( cur == slen ) return 0;
-            while( isdigit(str[cur]) and cur < slen)
-            {
-                out<<str[cur++];
-            }
-            cout<<out.str()<<endl;
-        }
-        else if(isdigit(str[cur]))
-        {
-            while(str[cur] == '0' and cur < slen)
-            ++cur;
-            if( cur == slen ) return 0;
-            while( isdigit(str[cur]) and cur < slen)
-            {
-                out<<str[cur++];
-            }
-            cout<<out.str()<<endl;
-        }
-        else return 0;
+        size_t index = str.find_first_not_of(' ');
         int64_t sum = 0;
-        string temp = out.str();
-        size_t tlen = temp.length();
-        for(int i = tlen-1; i >= 0; i--)
+        while( index < slen)
         {
-            int64_t y = (temp[i]-48)*pow(10, tlen-1-i);
-            sum += y;//(temp[i]-48)*pow(10, tlen-1-i);
+            if( str[index] == '+' || str[index] == '-')
+            {
+                plus = str[index] == '-' ? -1 : 1;
+                ++index;            
+            }
+            //此处加了else,就无法处理 +-2 这样的test case
+            while(isdigit(str[index]))
+            {
+                sum = sum*10+(str[index]-'0');
+                index++;
+                if(sum > INT_MAX)
+                return (plus==1) ? INT_MAX: INT_MIN;
+            }
+            return sum*plus;    
         }
-        if(flag == '-')
-        {
-            if( tlen > 10 || sum >2147483648)
-            return -2147483648;
-            else if( sum <= 2147483648)
-            return -1*sum;            
-        }
-        else if( flag == '+')
-        {
-            if( tlen > 10 || sum > 2147483647)
-            return 2147483647;
-            else if( sum <= 2147483647)
-            return sum;            
-        }
+        return sum;
+        
     }
 };
 int main(int argc, char const *argv[])
@@ -71,8 +38,12 @@ int main(int argc, char const *argv[])
     //-2147483648
     //2147483647
     Solution sol;
-    string str="    0000000000000   ";
-    // cout<<INT32_MIN<<endl<<INT32_MAX<<endl;
-    cout<<sol.myAtoi(str);
+    cout<<sol.myAtoi("    0000000000000   ")<<endl;
+    cout<<sol.myAtoi("+-2")<<endl;
+    cout<<sol.myAtoi("0-1")<<endl;
+    cout<<sol.myAtoi("+2147483648")<<endl;
+    cout<<sol.myAtoi("-2147483648")<<endl;
+    cout<<sol.myAtoi("+2147483649")<<endl;
+    cout<<sol.myAtoi("-2147483649")<<endl;
     return 0;
 }
