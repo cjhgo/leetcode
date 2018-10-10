7f4ae1389d1a7d66173de960e1f36268ecb512ab
chen <1728533217en@gmail.com>
2018-10-10 23:47:16 +0800

    update

---
+++ 014_Longest_Common_Prefix.cpp
@@ -0,0 +1,17 @@
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution 
+{
+public:
+    string longestCommonPrefix(vector<string>& strs) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
