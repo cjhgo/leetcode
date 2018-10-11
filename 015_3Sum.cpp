d46f7cf81770403632bee0175422e5fa782bd3cf
chen <1728533217en@gmail.com>
2018-10-12 01:05:05 +0800

    eliminate vector copy

--- 015_3Sum.cpp
+++ 015_3Sum.cpp
@@ -1,6 +1,6 @@
 #include<vector>
 #include<set>
-#include<tuple>
+#include<iterator>
 #include<algorithm>
 #include<map>
 #include<iostream>
@@ -8,15 +8,17 @@ using namespace std;
 class Solution 
 {
 public:
-    set<vector<int>> twoSum(vector<int>& nums, int target, bool& flag) 
+    set<vector<int>> twoSum(vector<int>& nums, int index, bool& flag) 
     {
         set< vector<int>> ret;        
         size_t n = nums.size();
         vector<int> result;
         map<int,int> value_to_index;
         flag=false;
+        int target = 0 - nums[index];
         for(int i = 0;i < n; i++)
         {
+            if(i == index) continue;
             vector<int> result;
             int to_find = target-nums[i];
             auto res = value_to_index.find(to_find);
@@ -40,12 +42,10 @@ public:
         set<vector<int>>  result;
         for(auto it = nums.begin(); it != nums.end(); it++)
         {
-            vector<int>temp, temp2;
+            vector<int>temp;
             int target = 0 - *it;
-            temp2.insert(temp2.begin(), nums.begin(),it);
-            temp2.insert(temp2.end(), it+1,nums.end());
             bool flag;
-            auto ret = twoSum(temp2, target, flag);
+            auto ret = twoSum(nums, std::distance(nums.begin(), it), flag);
             if(flag)
             {
                 result.insert(ret.begin(), ret.end());
