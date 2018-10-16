4db524d1847c6cb32729627d998dafc1b703d210
chen <1728533217en@gmail.com>
2018-10-16 23:35:42 +0800

    my method

--- 015_3Sum.cpp
+++ 015_3Sum.cpp
@@ -12,6 +12,47 @@ class Solution
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
+        if(nums.size() < 3) return {};
+        if(count(nums.begin(), nums.end(), 0) == nums.size())
+        return {{0,0,0}};
+        unordered_map<int,int> values;
+        for(auto it : nums)
+        {
+            values[it]++;
+        }
+        set<vector<int>>  result;
+        for(size_t i = 0; i < nums.size()-1; i++)
+        {
+            for(size_t j = i+1; j < nums.size();j++)
+            {
+                int a = nums[i], b = nums[j], c = 0- a-b;    
+                if(values.find(c) != values.end())
+                {
+                    if(c==a || c==b)
+                    {
+                        if( a == b)
+                        {
+                            //a==b==c,=0
+                            if(values.at(c) < 3)
+                            continue;
+                        }
+                        else
+                        {
+                            //a, b==c or b, a==c
+                            if(values.at(c) < 2)
+                            continue;
+                        }
+                    }
+                    int temp[]={a,b,c};
+                    sort(temp, temp+3);
+                    result.insert({temp, temp+3});
+                }
+            }
+        }        
+        return {result.begin(), result.end()};
+    }
+    vector<vector<int>> threeSum1(vector<int>& nums) 
+    {
         set<vector<int>>  result;
         if(nums.size() < 3) return {};
         if(count(nums.begin(), nums.end(), 0) == nums.size())
