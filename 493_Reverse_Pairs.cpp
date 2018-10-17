e42e9e9a58eed7d92bea2678d46f9de0b7e3bf9f
chen <1728533217en@gmail.com>
2018-10-17 23:50:42 +0800

    improve

--- 493_Reverse_Pairs.cpp
+++ 493_Reverse_Pairs.cpp
@@ -4,7 +4,35 @@ using namespace std;
 class Solution 
 {
 public:
+
+    int helper(vector<int>& nums, size_t left, size_t right);
+    {
+
+    }
+    int reverse(vector<int>& nums, size_t left, size_t right)
+    {
+        if(left+1 = right)
+        {
+            if(nums[left] > (int64_t)nums[right]*2)
+            return 1;
+            else
+            return 0;
+        }
+        else
+        {
+            int middle = left+right/2;
+            int left = reverse(nums, left, middle);
+            int right = reverse(nums, middle+1, right);
+            int both = helper(nums, left, right);
+            return left+right+both;
+        }
+
+    }
     int reversePairs(vector<int>& nums) 
+    {
+        return reverse(nums, 0, nums.size()-1);
+    }
+    int reversePairs2(vector<int>& nums) 
     {
         int count = 0;
         for(size_t i = 0; i < nums.size()-1; i++)
