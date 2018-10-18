e03e26dc4a033a8058a87889f92dc46a54a2f886
chen <1728533217en@gmail.com>
2018-10-18 23:57:43 +0800

    not finish 016

---
+++ 016_3Sum_Closest.cpp
@@ -0,0 +1,17 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+class Solution 
+{
+public:
+    int threeSumClosest(vector<int>& nums, int target) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    vector<int> nums ={-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+    return 0;
+}
