fa5b5b698b1e522a662f67f122006cfadfc98262
chen <1728533217en@gmail.com>
2018-10-24 00:12:54 +0800

    refine 017

--- 017_Letter_Combinations_of_a_Phone_Number.cpp
+++ 017_Letter_Combinations_of_a_Phone_Number.cpp
@@ -16,41 +16,6 @@ public:
         {'8', { "t", "u", "v"}},
         {'9', { "w", "x", "y", "z"}}
     };
-    vector<string> combinatons(string digits)
-    {
-        if(digits.size() == 2)
-        {
-            vector<string> temp;
-            for(auto c : num_2_chars[ digits[0] ])
-            {            
-                for(auto d : num_2_chars[ digits[1] ])
-                {
-                    string s;
-                    s += c;
-                    s += d;
-                    temp.push_back(s);
-                }
-            }
-            return temp;
-        }
-        else
-        {
-            string sub = digits.substr(1, digits.size()-1);
-            auto res = combinatons(sub);
-            vector<string> temp;
-            for(auto c : num_2_chars[ digits[0] ])
-            {
-                for(string _ : res)
-                {
-                    string s;                    
-                    s += c;
-                    s += _;
-                    temp.push_back(s);
-                }
-            }
-            return temp;
-        }
-    }
     vector<string> letterCombinations(string digits) 
     {
         if(digits.size() == 0)
@@ -61,19 +26,42 @@ public:
         {
             return num_2_chars[ digits[0] ];
         }
+        if(digits.size() == 2)
+        {
+            vector<string> temp;
+            for(auto c : num_2_chars[ digits[0] ])
+            {            
+                for(auto d : num_2_chars[ digits[1] ])
+                {
+                    temp.push_back(c+d);
+                }
+            }
+            return temp;
+        }
         else
         {
-            return combinatons(digits);
-        }
-        
+            //string sub = digits.substr(1, digits.size()-1);
+            string sub = digits.substr(1);
+            auto res = letterCombinations(sub);
+            vector<string> temp;
+            for(auto c : num_2_chars[ digits[0] ])
+            {
+                for(string _ : res)
+                {
+                    temp.push_back(c+_);
+                }
+            }
+            return temp;
+        }        
     }
 };
 
 int main(int argc, char const *argv[])
 {
     Solution sol;
-    string digits("2349");
+    string digits("2349786");
     auto res = sol.letterCombinations(digits);
     cout<<res.size()<<endl;
+    cout<<digits.substr(2)<<endl;
     return 0;
 }
