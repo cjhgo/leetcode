cabe35b4a8218b2d2010973ffa132fb5d3537e31
chen <1728533217en@gmail.com>
2018-10-26 20:37:52 +0800

    update

---
+++ 020_Valid_Parentheses.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        stack<char> s;       
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    string s("{[]}");
+    cout<<sol.isValid(s);
+    return 0;
+}
---
+++ 022_Generate_Parentheses.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    vector<string> generateParenthesis(int n) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    return 0;
+}
