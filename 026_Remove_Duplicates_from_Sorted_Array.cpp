fb240d3160e449b5ea4e8f970b7eeb06144deb2b
chen <1728533217en@gmail.com>
2018-10-31 22:13:00 +0800

    problem 026 Ac

---
+++ 026_Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    int removeDuplicates(vector<int>& nums) 
+    {
+        int counter = 1;
+        for(size_t i = 1; i < nums.size(); i++)
+        {
+            if( nums[i] != nums[i-1])
+            {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+        return counter;
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    vector<int> nums = {0,0,1,1,1,2,2,3,3,4};    
+    cout<<sol.removeDuplicates(nums)<<endl;
+    for(auto e: nums)
+    cout<<e<<"\t";
+    return 0;
+}
