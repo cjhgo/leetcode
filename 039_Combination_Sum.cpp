42a35a5b23bede11a8c5297a30968b292dd3dfd9
chen <1728533217en@gmail.com>
2018-11-05 23:58:31 +0800

    problem 039 Ac with poor performance

--- 039_Combination_Sum.cpp
+++ 039_Combination_Sum.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<set>
 #include<numeric>
 #include<vector>
 #include<string>
@@ -15,62 +16,39 @@ public:
     vector<vector<int>> helper(vector<int> nums, int target)
     {    
         vector<vector<int>> ret{{}};
-        cout<<target<<":\t";
-        for(auto e: nums)
-        {
-            cout<<e<<"\t";
-        }
-        cout<<endl<<endl;
-        if( nums.size() >= 2)
-        {  
-            int a = nums[0];
-            size_t quotient = target / a;
-            
-            for(size_t i = 0; i <= quotient; i++)
+        int a = nums[0];
+        size_t quotient = target / a;
+        
+        for(size_t i = 0; i <= quotient; i++)
+        {            
+            vector<int> temp(i, a);            
+            if( nums.size() > 1)
             {
-                
-                vector<int> temp(i, a);
                 size_t new_target = target - (i*a);
                 auto res = helper({nums.begin()+1, nums.end()}, new_target);
-                if( new_target  == 0  )
+                for(auto e : res)
                 {
-                    ret.push_back(temp);
-                }
-                else // new_target > 0
-                {                
-                    if(res.size() > 0)
-                    {
-                        for(auto e : res)
-                        {
-                            e.insert(e.end(),temp.begin(), temp.end());
-                            ret.push_back(e);
-                        }
-                    }
-                    else
-                    {
-
-                    }
+                    e.insert(e.end(),temp.begin(), temp.end());
+                    ret.push_back(e);
                 }
             }
-        }
-        else
-        {
-            if( nums[0] == target)
-            return {{target}};
             else
-            return {{}};
-        }
+                ret.push_back(temp);
+
+        }        
         return ret;
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
     {
-        vector<vector<int>> ret{{}};
+        set<vector<int>> ret;
         for(auto e: helper(candidates, target))
         {
             if( accumulate(e.begin(), e.end(),0) == target)
-            ret.push_back(e);
+            {
+                ret.insert(e);
+            }            
         }
-        return ret;
+        return {ret.begin(), ret.end()};
     }
 };
 int main(int argc, char const *argv[])
@@ -78,5 +56,12 @@ int main(int argc, char const *argv[])
     Solution sol;
     vector<int> nums{2,3,6,7};
     auto r = sol.combinationSum(nums, 7);
+    for(auto x : r)
+    {
+        for(auto xx: x)
+        cout<<xx<<"\t";
+        cout<<endl;
+    }        
+    cout<<r.size()<<endl;
     return 0;
 }
