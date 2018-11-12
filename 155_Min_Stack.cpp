d178cf6188ecb96cc1cafc9df682f88043f1592a
cjhgo <1728533217en@gmail.com>
2018-11-12 23:55:46 +0800

    Update 155_Min_Stack.cpp

--- 155_Min_Stack.cpp
+++ 155_Min_Stack.cpp
@@ -1,1 +1,38 @@
+#include<vector>
+using namespace std;
+class MinStack 
+{
+private:
+    vector<int> data;
+    int min;
+public:
+    /** initialize your data structure here. */
+    MinStack()
+    :min(2147483647)
+    {
+            
+    }
+    
+    void push(int x) 
+    {
+        data.push_back(x);
+        if( x < min)
+        min = x;
+    }
+    
+    void pop() 
+    {
+        data.pop_back();
+    }
+    
+    int top() 
+    {
+        return data[data.size()-1];
+    }
+    
+    int getMin() 
+    {
+        return min;
+    }
+};
 
