fcc80739277d5060db1606f22b895da6e8f7faed
cjhgo <1728533217en@gmail.com>
2018-11-14 06:33:11 -0600

    Update 155_Min_Stack.cpp

--- 155_Min_Stack.cpp
+++ 155_Min_Stack.cpp
@@ -1,33 +1,100 @@
 #include<vector>
-#include <algorithm>
+#include<iostream>
 using namespace std;
-class MinStack 
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class MyStack
 {
 private:
     vector<int> data;
+    int mysize;
 public:
-    MinStack()
+    MyStack():
+    mysize(0),data(100,0)
     {
-            
     }
     
     void push(int x) 
     {
-        data.push_back(x);
+        if(mysize == data.size())
+        {
+            data.resize(data.size()+100);
+        }
+        data[mysize] = x;
+        mysize++;        
     }
     
     void pop() 
     {
-        data.pop_back();
+        mysize--;
     }
     
     int top() 
     {
-        return data[data.size()-1];
+        return data[mysize-1];
+    }
+    bool empty()
+    {
+        return mysize == 0;
+    }
+};
+
+class MinStack 
+{
+private:
+    MyStack sdata,smin;
+public:
+    /** initialize your data structure here. */
+    MinStack() 
+    {
+        
+    }
+    
+    void push(int x) 
+    {
+        sdata.push(x);
+        if(not smin.empty())
+        {
+            if( x <= smin.top())
+                smin.push(x);
+        }
+        else
+            smin.push(x);
+    }
+    
+    void pop() 
+    {
+        if( sdata.empty() || smin.empty())
+            return;
+        int x = sdata.top();
+        sdata.pop();
+        if( x == smin.top())
+            smin.pop();
+    
+    }
+    
+    int top() 
+    {
+        return sdata.top();   
     }
     
     int getMin() 
     {
-        return *min_element(data.begin(), data.end());
+        return smin.top();
     }
 };
+
+int main() 
+{
+    MinStack ms;
+    ms.push(1);
+    ms.push(-1);
+    ms.push(-1);
+    cout<<ms.getMin()<<stdl;
+    ms.pop();
+    cout<<ms.getMin()<<stdl;
+    return 0;
+}
