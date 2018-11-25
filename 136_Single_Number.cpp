eb731eae92c4b8d01401e433cdbfa7290cbf5bed
chen <1728533217en@gmail.com>
2018-11-25 23:53:48 +0800

    add 136

---
+++ 136_Single_Number.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        int64_t flag;
+        for(auto e : nums)    
+        {
+            
+        }
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    return 0;
+}
