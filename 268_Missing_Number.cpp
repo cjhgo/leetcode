c6c0ce16b5d64169fc5f63464bbd872d26e93b63
chen <1728533217en@gmail.com>
2018-11-28 02:02:07 +0800

    update

--- 268_Missing_Number.cpp
+++ 268_Missing_Number.cpp
@@ -13,11 +13,19 @@ class Solution
 public:
     int missingNumber(vector<int>& nums) 
     {
-        
+        int sum = 0;
+        for(auto e : nums)
+        {
+            sum += e;
+        }
+        int expect_sum = (0 + nums.size())*(nums.size() +1)/2;
+        return expect_sum - sum;
     }
 };
 int main(int argc, char const *argv[])
 {
     Solution sol;
+    vector<int> nums = {9,6,4,2,3,5,7,0,1};
+    cout<<sol.missingNumber(nums);
     return 0;
 }
