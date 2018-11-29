d446fc8567a8bc1be2eafa7e9b5e14f8460f7a12
chen <1728533217en@gmail.com>
2018-11-30 01:11:28 +0800

    update

--- 070_Climbing_Stairs.cpp
+++ 070_Climbing_Stairs.cpp
@@ -13,11 +13,21 @@ class Solution
 public:
     int climbStairs(int n) 
     {
-        
+        vector<int> v = {0,1,2};
+        v.resize(n+1);
+        if( n >= 3)
+        {
+            for(size_t j = 3; j<= n;j++)
+            {
+                v[j] = v[j-2]+v[j-1];
+            }
+        }
+        return v[n];
     }
 };
 int main(int argc, char const *argv[])
 {
     Solution sol;
+    cout<<sol.climbStairs(6);
     return 0;
 }
