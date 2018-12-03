a06a3e6f03a39e5869a81f5055923b56ff7138fd
chen <1728533217en@gmail.com>
2018-12-03 23:39:56 +0800

    add 121

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    
+    return 0;
+}
