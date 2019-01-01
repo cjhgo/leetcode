ec7bfb84175654bf2feb2e05042f82dc4af3cd2c
chen <1728533217en@gmail.com>
2019-01-01 16:26:45 +0800

    update

---
+++ 310_Minimum_Height_Trees.cpp
@@ -0,0 +1,23 @@
+#include"struct_define.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
