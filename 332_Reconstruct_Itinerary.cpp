3fe82aafbe61fee0332f0e18e694efe33c4db14a
chen <1728533217en@gmail.com>
2019-01-02 23:21:55 +0800

    update

---
+++ 332_Reconstruct_Itinerary.cpp
@@ -0,0 +1,23 @@
+#include"struct_define.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    vector<string> findItinerary(vector<pair<string, string>> tickets) 
+    {        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    return 0;
+}
