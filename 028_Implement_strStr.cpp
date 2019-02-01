49205e973f2fbec97fdd8cd53bee43a30ac2f903
chen <1728533217en@gmail.com>
2019-02-01 23:27:46 +0800

    update

---
+++ 028_Implement_strStr.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    int strStr(string haystack, string needle) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    return 0;
+}
