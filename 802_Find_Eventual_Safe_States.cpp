e8b494ecf715fd7641ba4055a7ed693f8b31f5f9
chen <1728533217en@gmail.com>
2019-02-05 02:56:09 +0800

    update

--- 802_Find_Eventual_Safe_States.cpp
+++ 802_Find_Eventual_Safe_States.cpp
@@ -1,6 +1,7 @@
 #include"struct_define.h"
 #include<iostream>
 #include<vector>
+#include<stack>
 #include<string>
 #include<map>
 using namespace std;
@@ -15,6 +16,25 @@ public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
         
+    }
+    bool find_cycle_bydfs(int source, vector<vector<int>>& graph)
+    {
+        enum Color {WHITE, GRAY, BLACK};
+        size_t len=graph.size();
+        vector<Color> colors (len, WHITE);
+        stack<int> mystack;
+        colors[source]=GRAY;
+        vector<int> visited(len, 0);
+        visited[source] = 1;
+        mystack.push(source);        
+        while( not mystack.empty() )
+        {
+            if( graph[mystack.top()].size() == 0 )
+            {
+                cout<<mystack.top()<<std::endl;
+                mystack.pop();
+            }
+        }
     }
 };
 int main(int argc, char const *argv[])
