9801addf82ac2cb0cfa6f9fbf31a053dcf279185
chen <1728533217en@gmail.com>
2019-02-05 21:13:54 +0800

    problem 802 Ac,,,

--- 802_Find_Eventual_Safe_States.cpp
+++ 802_Find_Eventual_Safe_States.cpp
@@ -15,30 +15,56 @@ class Solution
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
-        
+        vector<int> res;
+
+        for(size_t i = 0; i < graph.size(); i++)
+        {
+            if( not find_cycle_bydfs(i, graph) )
+            res.push_back(i);
+        }
+        return res;
     }
     bool find_cycle_bydfs(int source, vector<vector<int>>& graph)
     {
-        enum Color {WHITE, GRAY, BLACK};
         size_t len=graph.size();
-        vector<Color> colors (len, WHITE);
-        stack<int> mystack;
-        colors[source]=GRAY;
         vector<int> visited(len, 0);
-        visited[source] = 1;
-        mystack.push(source);        
-        while( not mystack.empty() )
+        return dfs(source, visited, graph);
+    }
+    bool dfs(int source, vector<int>& visited, vector<vector<int>>& graph)
+    {        
+        if( visited[source] == 0 )
         {
-            if( graph[mystack.top()].size() == 0 )
+            cout<<source<<"\t";
+            visited[source]=1;
+        }
+        for(auto e : graph[source])
+        {
+            if( visited[e] == 1 )
             {
-                cout<<mystack.top()<<std::endl;
-                mystack.pop();
+                cout<<"find cycle! while processing "<<source<<"->"<<e<<endl;
+                return true;
+            }
+            if( visited[e] == 0)
+            {
+                if( dfs(e, visited, graph))
+                return true;
             }
         }
+        visited[source]=2;
+        return false;
     }
 };
 int main(int argc, char const *argv[])
 {
     Solution sol;
+    vector<vector<int> > graph = {
+        {{1,2},{2,3},{5},{0},{5},{},{}}
+    };    
+    auto res = sol.eventualSafeNodes(graph);
+    cout<<endl<<endl;
+    for(auto e :  res)
+    {
+        cout<<e<<"\t";
+    }
     return 0;
 }
