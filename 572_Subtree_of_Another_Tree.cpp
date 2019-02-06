15e7f537e2d9bfcde455ba62e58ea972262b2460
chen <1728533217en@gmail.com>
2019-02-06 20:23:58 +0800

    problem 572 Ac

--- 572_Subtree_of_Another_Tree.cpp
+++ 572_Subtree_of_Another_Tree.cpp
@@ -9,8 +9,39 @@ static const auto x=[](){
     std:cin.tie(nullptr);
     return nullptr;
 }(); 
+class Solution {
+public:
+    bool isIdentical(TreeNode* s, TreeNode* t)
+    {
+        if( s==nullptr and t==nullptr)
+            return true;
+        else if( s!=nullptr and t!=nullptr)
+        {
+            if(s->val == t->val and (isIdentical(s->left, t->left) and isIdentical(s->right, t->right) ) )
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+    bool isSubtree(TreeNode* s, TreeNode* t) 
+    {
+        if( s==nullptr and t==nullptr)
+            return true;
+        else if( s!=nullptr and t!=nullptr)
+        {
+            if( isIdentical(s, t))
+                return true;
+            else
+                return isSubtree(s->left, t) or isSubtree(s->right, t);                
+        }
+        else
+            return false;
+    }
+};
 int main(int argc, char const *argv[])
 {
-    Solution sol;
+    Solution sol;    
     return 0;
 }
