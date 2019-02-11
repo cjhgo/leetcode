850a49237cfaf57c652ac4654c4e556edfd7719b
chen <1728533217en@gmail.com>
2019-02-11 23:16:08 +0800

    update

---
+++ 785_Is_Graph_Bipartite.cpp
@@ -0,0 +1,23 @@
+#include"struct_define.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+    std::ios::sync_with_stdio(false);
+    std:cin.tie(nullptr);
+    return nullptr;
+}(); 
+class Solution 
+{
+public:
+    bool isBipartite(vector<vector<int>>& graph) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
