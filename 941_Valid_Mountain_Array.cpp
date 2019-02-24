50457031ae60b25ba236b8a170c225a8f9944938
chen <1728533217en@gmail.com>
2019-02-24 18:05:57 +0800

    problem 941 Ac

--- 941_Valid_Mountain_Array.cpp
+++ 941_Valid_Mountain_Array.cpp
@@ -13,7 +13,21 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& A) 
     {
-        
+        if(A.size() < 3 || A[1] <= A[0]) return false;
+        size_t i = 1;
+        for(; i < A.size(); i++)
+        {
+            if( A[i-1] < A[i])continue;
+            else if( A[i-1] == A[i]) return false;
+            else break;
+        }
+        if(i == A.size()) return false;
+        for(; i <A.size(); i++)
+        {
+            if( A[i-1] > A[i]  )continue;
+            else return false;
+        }
+        return true;
     }
 };
 int main(int argc, char const *argv[])
