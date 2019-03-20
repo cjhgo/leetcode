db07f61941e394f9a8b37e6430e2ab345d7802d9
chen <1728533217en@gmail.com>
2019-03-20 13:36:55 +0800

    update

--- golang/029_Divide_Two_Integers.go
+++ golang/029_Divide_Two_Integers.go
@@ -2,42 +2,42 @@ package main
 import "fmt"
 
 func divide_unsign(dividend int, divisor int) int {
-	var i = 0
-	var c = 1
-	var sub = divisor
-	for;dividend >= divisor;{
-		if( dividend >= sub){
-			dividend -= sub
-			i += c
-			sub = (sub << 1)
-			c = (c<<1)
-		}else
-		{
-			sub = (sub>>1)
-			c = (c>>1)
-		}
-	}
-	return i
+  var i = 0
+  var c = 1
+  var sub = divisor
+  for;dividend >= divisor;{
+    if( dividend >= sub){
+      dividend -= sub
+      i += c
+      sub = (sub << 1)
+      c = (c<<1)
+    }else
+    {
+      sub = (sub>>1)
+      c = (c>>1)
+    }
+  }
+  return i
 }
 func Abs(x int) int {
-	if x < 0 {
-		return -x
-	}
-	return x
+  if x < 0 {
+    return -x
+  }
+  return x
 }
 func divide(dividend int, divisor int) int {
-	var sign = (dividend>0) && (divisor < 0) || (dividend < 0)  && (divisor >0)
-	if sign{
-		return -1*divide_unsign(Abs(dividend),Abs(divisor))
-	}else{
-		var res=divide_unsign(Abs(dividend),Abs(divisor))
-		if( res == 2147483648){
-			return 2147483647
-		}else{
-			return res
+  var sign = (dividend>0) && (divisor < 0) || (dividend < 0)  && (divisor >0)
+  if sign{
+    return -1*divide_unsign(Abs(dividend),Abs(divisor))
+  }else{
+    var res=divide_unsign(Abs(dividend),Abs(divisor))
+    if( res == 2147483648){
+      return 2147483647
+    }else{
+      return res
+    }    
 		}		
-	}
-}
+}    
 func main() {
 	fmt.Println(divide(10,3))
 	fmt.Println(divide(7,-3))
--- golang/058_Length_of_Last_Word.go
+++ golang/058_Length_of_Last_Word.go
@@ -1,2 +1,35 @@
-package golang
+package main
+import "fmt"
+import "strings"
+
+func lengthOfLastWord(s string) int {
+  var i = 0
+  var sum =0
+  for i = len(s)-1; i >=0; i--{
+	  if(s[i]!=' '){
+		break
+	  }	  
+  }
+  for ; i >=0; i--{
+	if(s[i]!=' '){
+		sum++
+	}else{
+		break
+	}
+
+  }
+  return sum
+}
+
+func main(){
+  str:="hello world"
+  fmt.Println(str)
+  fmt.Println(strings.Index(str, " "))
+  fmt.Println(strings.Index(str, "X"))
+  fmt.Println(len(str))
+  fmt.Println(lengthOfLastWord(str))
+  fmt.Println(lengthOfLastWord("a "))
+  fmt.Println(lengthOfLastWord("a"))
+
+}
 
