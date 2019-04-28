8b290ca6c9bb1f3abf657a0ffd249086d2d17fe5
chen <1728533217en@gmail.com>
2019-04-28 16:54:03 +0800

    fix 032

--- 032_Longest_Valid_Parentheses.cpp
+++ 032_Longest_Valid_Parentheses.cpp
@@ -17,40 +17,26 @@ public:
     {
       if(s.size() < 2) return 0;
       int longest=0;
-      typedef pair<char,size_t> ele_type;
-      stack<ele_type> match;
-      size_t i = 0;
-      match.push({s[i],i});
-      while(not  match.empty() || (i+1) < s.size())
-      {        
-        if( i+1 ==s.size())break;
-        ele_type next{s[i+1],i+1};
-        if( not match.empty() && match.top().first == '(' && next.first==')')
+
+      stack<int> mystack;
+      mystack.push(-1);
+      for(size_t i = 0 ; i < s.size();i++)
+      {
+        if( s[i] =='(')
+          mystack.push(i);
+        else
         {
-          match.pop();
-        }else
-        {
-          match.push(next);
-        }          
-        i++;                
-      }
-      if (match.empty())return s.size();
-      else
-      {        
-        vector<size_t> not_match;        
-        while(not match.empty())
-        {        
-          not_match.push_back(match.top().second);
-          match.pop();
-        }  
-        int temp =s.size()-1 - not_match[0];        
-        if(  temp > longest) longest=temp;
-        temp = not_match[not_match.size()-1]-0;
-        if(  temp > longest) longest=temp;
-        for(int i = 0;i+1 <= not_match.size()-1;i++)
-        {
-          temp = not_match[i]-not_match[i+1]-1;
-          if(  temp > longest) longest=temp;
+          mystack.pop();
+          if(mystack.empty())
+          {
+            mystack.push(i);
+          }
+          else
+          {
+            int temp = i-mystack.top();
+            if(temp > longest)
+            longest=temp;
+          }
         }
       }
       return longest;
