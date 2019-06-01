69c48616176a54f65f3383c715241c76669fd932
chen <1728533217en@gmail.com>
2019-06-01 18:17:52 +0800

    update subset i,ii

--- 078_Subsets.cpp
+++ 078_Subsets.cpp
@@ -2,11 +2,12 @@
 #include<bitset>
 #include<vector>
 #include<cmath>
+#include <algorithm>
 using namespace std;
 class Solution
 {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) 
+    vector<vector<int>> subsets2(vector<int>& nums) 
     {
         const size_t n = pow(2, nums.size());
         vector<vector<int>> res;
@@ -25,6 +26,27 @@ public:
         cout<<n<<"#"<<res.size()<<endl;
         return res;
     }
+    
+    void helper(vector<int>& nums, int cur, int from)
+    {
+        res.push_back({temp.begin(), temp.begin()+cur});
+        for(int i = from; i < nums.size();i++)
+        {
+            if(i>from &&nums[i] == nums[i-1])continue; //this line is for subsetsWithDup
+            temp[cur] = nums[i];
+            helper(nums, cur+1, i+1);
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums)     
+    {
+        sort(nums.begin(), nums.end());
+        temp.resize(nums.size());
+        helper(nums,0,0);
+        return res;
+    }
+private:
+    vector<vector<int>> res;
+    vector<int> temp;
 };
 int main(int argc, char const *argv[])
 {
@@ -34,6 +56,8 @@ int main(int argc, char const *argv[])
     nums.push_back(i);
     Solution s;
     
+    nums = {3,5,7,7,9};
+    // nums = {1,2,2};
     for(auto e: s.subsets(nums))
     {
         for(auto x:e)
