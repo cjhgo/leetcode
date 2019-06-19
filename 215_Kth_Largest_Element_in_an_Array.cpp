57b289accd395f76232ef9fb5ab6d4858a44529a
chen <1728533217en@gmail.com>
2019-06-19 22:26:11 +0800

    update

--- 215_Kth_Largest_Element_in_an_Array.cpp
+++ 215_Kth_Largest_Element_in_an_Array.cpp
@@ -13,16 +13,57 @@ static const auto x=[](){
 class Solution 
 {
 public:
-    int findKthLargest(vector<int>& nums, int k) 
+    int findKthLargest1(vector<int>& nums, int k) 
     {
+        //逆序增序,即顺序降序
         sort(nums.rbegin(), nums.rend());
         return nums[k-1];
     }
+    int findKthLargest2(vector<int>& nums, int k) 
+    {
+        //第k大的元素就是升序中第 size-k个元素,
+        //比如5个元素中第2大的就是升序中第5-2个元素
+        std::nth_element(nums.begin(),nums.begin()+nums.size()-k,nums.end());
+        return nums[nums.size()-k];
+    }
+
+    void findKthByPart(vector<int>::iterator first, vector<int>::iterator last, vector<int>& nums, int k0)
+    {   
+        int pivot = *first;
+        auto it = std::partition(first,last, [pivot](int e){return e > pivot;});        
+        auto m = distance(first, it);         
+        if( m == 0 & k0 == 0)return;
+        if( k0 < m)
+            findKthByPart(first, it, nums,k0);
+        else//k0 >= m
+        {
+            if(m== 0)
+            findKthByPart(it+1, last, nums,k0-m-1);
+            else
+            findKthByPart(it, last, nums,k0-m);
+        }                    
+    }
+    int findKthLargest(vector<int>& nums, int k) 
+    {
+        findKthByPart(nums.begin(),nums.end(),nums,k-1);
+        return nums[k-1];    
+    }
+    
 };
 int main(int argc, char const *argv[])
 {
-    vector<int> nums = {3,2,3,1,2,4,5,5,6};
+    vector<int> nums = {3,2,3,1,2,4,5,5,6}, nums2=nums;
     Solution sol;
-    cout<<sol.findKthLargest(nums,4);
+    cout<<sol.findKthLargest2(nums,4);
+    cout<<endl;
+    cout<<sol.findKthLargest(nums2,4)<<endl;
+    nums2={3,3,3,3,4,3,3,3,3};
+    cout<<sol.findKthLargest(nums2,9)<<endl;    
+    nums2={2,1};
+    cout<<sol.findKthLargest(nums2,1)<<endl;
+    nums2={-1,2,0};
+    cout<<sol.findKthLargest(nums2,1)<<endl;
+    nums2={3,1,2,4};
+    cout<<sol.findKthLargest(nums2,2)<<endl;    
     return 0;
 }
