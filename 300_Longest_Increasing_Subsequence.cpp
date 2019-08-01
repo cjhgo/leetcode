2390641322eeaf3a37703594098a102972dbef23
chen <1728533217en@gmail.com>
2019-08-01 18:05:49 +0800

    problem 300 Ac using dp with o(n^2)

--- 300_Longest_Increasing_Subsequence.cpp
+++ 300_Longest_Increasing_Subsequence.cpp
@@ -1,5 +1,6 @@
 #include"struct_define.h"
 #include<iostream>
+#include <algorithm>
 #include<vector>
 #include<string>
 #include<map>
@@ -9,18 +10,38 @@ static const auto x=[](){
   std:cin.tie(nullptr);
   return nullptr;
 }(); 
-class Solution 
+class Solution
 {
 public:
     int lengthOfLIS(vector<int>& nums) 
     {
-      
+      if(nums.size() < 2) return nums.size();
+      vector<int> dp(nums.size());
+      dp[0]=1;
+      for(int i = 1; i < nums.size(); i++)
+      {
+        int dpi = 1;
+        for(int j = 0; j < i; j++)
+        {
+          if(nums[i] > nums[j])
+          {
+            if(dp[j]+1 > dpi) dpi = dp[j]+1;
+          }
+        }
+        dp[i] = dpi;
+      }
+
+      return *std::max_element(dp.begin(),dp.end());
     }
+
 };
+
 int main(int argc, char const *argv[])
 {
   Solution sol;
-  vector<int> input = {10,9,2,5,3,7,101,18};
+  vector<int> input = {4,10,4,3,8,9};
+  //{2,2};
+  //{10,9,2,5,3,7,101,18};
   std::cout<<sol.lengthOfLIS(input);
   return 0;
 }
