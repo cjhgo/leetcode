45f858081fafd2e54d45c548222f591df142c8de
chen <1728533217en@gmail.com>
2019-08-07 22:51:53 +0800

    update

---
+++ 322_Coin_Change.cpp
@@ -0,0 +1,24 @@
+#include"struct_define.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+  std::ios::sync_with_stdio(false);
+  std:cin.tie(nullptr);
+  return nullptr;
+}(); 
+class Solution 
+{
+public:
+    int coinChange(vector<int>& coins, int amount) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+  Solution sol;
+  return 0;
+}
---
+++ 518_Coin_Change_2.cpp
@@ -0,0 +1,25 @@
+#include"struct_define.h"
+#include<iostream>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+static const auto x=[](){
+  std::ios::sync_with_stdio(false);
+  std:cin.tie(nullptr);
+  return nullptr;
+}(); 
+
+class Solution 
+{
+public:
+    int change(int amount, vector<int>& coins) 
+    {
+        
+    }
+};
+int main(int argc, char const *argv[])
+{
+  Solution sol;
+  return 0;
+}
