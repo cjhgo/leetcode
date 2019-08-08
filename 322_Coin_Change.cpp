307c706dbf032ee847b3eb9a889b33c3ff0ea970
chen <1728533217en@gmail.com>
2019-08-09 00:15:13 +0800

    update

--- 322_Coin_Change.cpp
+++ 322_Coin_Change.cpp
@@ -9,16 +9,44 @@ static const auto x=[](){
   std:cin.tie(nullptr);
   return nullptr;
 }(); 
+
 class Solution 
 {
 public:
-    int coinChange(vector<int>& coins, int amount) 
+    int coinChange(vector<int>& coins, int amount)
     {
-        
+      int n = coins.size();
+      int dp[n+1][amount+1];      
+      
+      for(int i = 0; i <=n; i++)
+      dp[i][0] = 0;
+      for(int i = 0; i <= amount; i++)
+      dp[0][i] = -1;
+      dp[0][0]=0;
+
+      for(int i = 1; i <= n; i++)
+      for(int j = 1; j <= amount; j++)
+      {
+        int temp = INT32_MAX;
+        for(int k = 0; k <=  j/coins[i-1];k++)
+        {
+          int temp2 = dp [i-1][j - k*coins[i-1] ];
+          if(temp2 >= 0 && temp2+k < temp)
+          {            
+            temp=temp2+k;
+          }                  
+        }
+        if(temp == INT32_MAX) dp[i][j]=-1;
+        else dp[i][j] = temp;
+      }
+      printMatrix<int>((int*)dp, n+1, amount+1);
+      return dp[n][amount];
     }
 };
 int main(int argc, char const *argv[])
 {
   Solution sol;
+  vector<int> coins = {1,2,5};
+  cout<<sol.coinChange(coins,11);
   return 0;
 }
--- 518_Coin_Change_2.cpp
+++ 518_Coin_Change_2.cpp
@@ -15,11 +15,34 @@ class Solution
 public:
     int change(int amount, vector<int>& coins) 
     {
-        
+      int n = coins.size();
+      int dp[n+1][amount+1];
+      vector<int> k_i(n);
+      
+      for(int i = 0; i <=n; i++)
+      dp[i][0] = 1;
+      for(int i = 0; i <= amount; i++)
+      dp[0][i] = 0;
+      dp[0][0]=1;
+
+      for(int i = 1; i <= n; i++)
+      for(int j = 1; j <= amount; j++)
+      {
+        int temp = 0;
+        for(int k = 0; k <=  j/coins[i-1];k++)
+        {
+          int temp2 = j - k*coins[i-1];
+          temp += dp[i-1][temp2];
+        }
+        dp[i][j] = temp;
+      }
+      return dp[n][amount];
     }
 };
 int main(int argc, char const *argv[])
 {
   Solution sol;
+  vector<int> coins = {1,2,5};
+  cout<<sol.change(5,coins);
   return 0;
 }
