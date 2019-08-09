704b077f9ca2b2ab0d6d558bd7bec06e0a6f0d84
chen <1728533217en@gmail.com>
2019-08-09 20:52:53 +0800

    update

--- 518_Coin_Change_2.cpp
+++ 518_Coin_Change_2.cpp
@@ -28,26 +28,57 @@ public:
       for(int i = 1; i <= n; i++)
       for(int j = 1; j <= amount; j++)
       {
-        /**
-         int temp = 0;
+        
+        int temp = 0;
         for(int k = 0; k <=  j/coins[i-1];k++)
         {
           int temp2 = j - k*coins[i-1];
           temp += dp[i-1][temp2];
         }
-        dp[i][j] = temp;
-        **/
+        dp[i][j] = temp;        
+
+      }
+      return dp[n][amount];
+    }//160ms
+    int change2(int amount, vector<int>& coins) 
+    {
+      int n = coins.size();
+      int dp[n+1][amount+1];
+      vector<int> k_i(n);
+      
+      for(int i = 0; i <=n; i++)
+      dp[i][0] = 1;
+      for(int i = 0; i <= amount; i++)
+      dp[0][i] = 0;
+      dp[0][0]=1;
+
+      for(int i = 1; i <= n; i++)
+      for(int j = 1; j <= amount; j++)
+      {
         dp[i][j] = dp[i-1][j];
         if( j - coins[i-1] >= 0)
         dp[i][j] += dp[i][j-coins[i-1]];
       }
       return dp[n][amount];
-    }
+    }//28ms
+    int change3(int amount, vector<int>& coins) 
+    {      
+      
+      vector<int>dp(amount+1,0);
+      dp[0]=1;
+      for(auto coin : coins)
+      for(int j = coin; j <= amount; j++)
+      {                
+        dp[j]+=dp[j-coin];        
+      }
+      return dp[amount];
+    }//8ms    
 };
 int main(int argc, char const *argv[])
 {
   Solution sol;
-  vector<int> coins = {1,2,5};
-  cout<<sol.change(5,coins);
+  vector<int> coins = {1,5,10,25};
+  cout<<sol.change(6,coins)<<endl;
+  cout<<sol.change(35837,coins)%1000000007;
   return 0;
 }
